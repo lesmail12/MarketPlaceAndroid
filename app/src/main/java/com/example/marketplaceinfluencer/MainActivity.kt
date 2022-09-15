e6077822e91c7af137af7581cf08260e4a857fef
user <agungismail444@gmail.com>
2022-09-15 12:28:55 +0700

    -Project Cloning Aplikasi

--- app/src/main/java/com/example/marketplaceinfluencer/MainActivity.kt
+++ app/src/main/java/com/example/marketplaceinfluencer/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.marketplaceinfluencer
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d( Tag: "RESPON" , msg: "PESAN SINGKAT" )
     }
 }
